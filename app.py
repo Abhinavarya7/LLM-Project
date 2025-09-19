e53ed995ed49d9b6cf578c39530ef92a4063364f
Abhinav Arya <101444107+Abhinavarya7@users.noreply.github.com>
2025-09-19 21:03:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def get_text_chunks(text):
         A list of text chunks.
     """
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=500)
     chunks = text_splitter.split_text(text)
     return chunks
 
@@ -179,6 +179,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
